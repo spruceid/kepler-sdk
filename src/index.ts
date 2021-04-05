9607e02789e5837a59b2587dbcf409f6190b5a26
Charles Cunningham <c.a.cunningham6@gmail.com>
2021-04-05 15:03:10 +0100

    initial kepler and orbit class impls

---
+++ src/index.ts
@@ -0,0 +1,91 @@
+import {
+    Signer
+} from '@taquito/taquito';
+import {
+    HttpBackend
+} from '@taquito/http-utils';
+
+enum Action {
+    get = "GET",
+    put = "PUT",
+    delete = "DEL"
+}
+
+const createRequest = (orbit: string, action: Action, pk: string, pkh: string, cid: string): string => {
+    return `Tezos Signed Message: ${orbit}.kepler.net ${Date.now()} ${pk} ${pkh} ${cid}`
+}
+
+const makePath = (url: string, orbit: string, cid: string) => url + "/" + orbit + "/" + cid
+
+export class Kepler<S extends Signer> {
+    constructor(
+        private url: string,
+        private signer: S,
+        private http: HttpBackend = new HttpBackend()
+    ) { }
+
+    public async get<T>(orbit: string, cid: string): Promise<T> {
+        return await this.http.createRequest({
+            url: makePath(this.url, orbit, cid),
+            method: 'GET',
+            headers: {
+                Authorization: await this.createAuth(orbit, cid, Action.get)
+            }
+        })
+    }
+
+    public async put<T>(content: T, orbit: string, cid?: string): Promise<void> {
+        return await this.http.createRequest<string>({
+            url: makePath(this.url, orbit, cid),
+            method: 'POST',
+            headers: {
+                Authorization: await this.createAuth(orbit, cid, Action.put)
+            }
+        }, content)
+    }
+
+    public async del(orbit: string, cid: string): Promise<void> {
+        return await this.http.createRequest({
+            url: makePath(this.url, orbit, cid),
+            method: 'DELETE',
+            headers: {
+                Authorization: await this.createAuth(orbit, cid, Action.delete)
+            }
+        })
+
+    }
+
+    public orbit(orbit: string) {
+        return new Orbit(this, orbit);
+    }
+
+    private async createAuth(orbit: string, cid: string, action: Action): Promise<string> {
+        const auth = createRequest(orbit, action, await this.signer.publicKey(), await this.signer.publicKeyHash(), cid);
+        const { prefixSig } = await this.signer.sign({ string: auth });
+
+        return auth + " " + prefixSig
+    }
+}
+
+export class Orbit<S extends Signer> {
+    constructor(
+        private kepler: Kepler<S>,
+        private orbitId: string
+    ) { }
+
+    public get orbit() {
+        return this.orbitId
+    }
+
+    public async get<T>(cid: string): Promise<T> {
+        return await this.kepler.get<T>(this.orbit, cid)
+    }
+
+    public async put<T>(content: T, cid?: string): Promise<void> {
+        return await this.kepler.put<T>(content, this.orbit, cid)
+    }
+
+    public async del(cid: string): Promise<void> {
+        return await this.kepler.del(this.orbit, cid)
+    }
+}
