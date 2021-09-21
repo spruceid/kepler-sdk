2d0b73c56684e1477af10d824525434b087ada27
Charles Cunningham <c.a.cunningham6@gmail.com>
2021-09-21 11:16:21 +0200

    remove /dist

--- dist/ethZcap.d.ts
+++
@@ -1,2 +0,0 @@
-import { Authenticator } from ".";
-export declare const ethZcapAuthenticator: (client: any, domain: string, prepareInvokeCapability: any, completeInvokeCapability: any) => Promise<Authenticator>;
