e59a8469e9b366f1748be2006211b1083dc182d6
Charles Cunningham <c.a.cunningham6@gmail.com>
2021-11-26 17:53:51 +0100

    just use an ethers Signer for SIWE

--- src/siwe.ts
+++ src/siwe.ts
@@ -1,52 +1,24 @@
 import { SiweMessage } from 'siwe';
-import { ExternalProvider } from '@ethersproject/providers';
+import { Signer } from 'ethers';
 import { Authenticator, Action, getOrbitId, orbitParams } from '.';
 
-type Provider = Required<Pick<ExternalProvider, 'request'>> & ExternalProvider;
 
-export const SIWEAuthenticator = async <P extends Provider>(client: P, domain: string): Promise<Authenticator> => {
-    if (typeof client.request !== 'function') {
-        throw new Error("Client doesnt implement Eth provider functionality")
-    }
-    const accounts = await client.request({ method: 'eth_accounts' });
-    const pkh = accounts ? accounts[0] : undefined;
-    if (!pkh || typeof pkh !== 'string') {
-        throw new Error("No Active Account")
-    }
+export const SIWEAuthenticator = async <S extends Signer>(client: S, domain: string): Promise<Authenticator> => {
+    const pkh = await client.getAddress();
 
     return {
         content: async (orbit: string, cids: string[], action: Action): Promise<HeadersInit> => {
             const auth = createSiweAuthContentMessage(orbit, pkh, action, cids, domain);
-            const signature = sign(client, new TextEncoder().encode(auth), pkh)
+            const signature = await client.signMessage(auth);
             return { "Authorization": auth + " " + signature }
         },
         createOrbit: async (cids: string[], params: { [key: string]: number | string }): Promise<{ headers: HeadersInit, oid: string }> => {
             const { oid, auth } = createSiweAuthCreationMessage(pkh, cids, domain, params)
-            const signature = await sign(client, new TextEncoder().encode(auth), pkh);
+            const signature = await client.signMessage(auth);
             return { headers: { "Authorization": auth + " " + signature }, oid }
         }
     }
 }
-
-const HEX = "0123456789abcdef";
-
-const hexEncode = (data: Uint8Array) => {
-    let r = '';
-    for (let i = 0; i < data.length; i++) {
-        const v = data[i];
-        r += HEX[(v & 0xf0) >> 4] + HEX[v & 0x0f];
-    }
-    return r
-}
-
-const sign = async <P extends Provider>(client: P, data: Uint8Array, address: string) =>
-    await client.request({
-        method: 'personal_sign',
-        params: ["0x" + hexEncode(data), address]
-    }).then(s => {
-        if (typeof s === 'string') return s;
-        else throw new Error("Failed to sign data")
-    })
 
 const statement = "Authorize an action on your Kepler Orbit";
 const version = "1";
