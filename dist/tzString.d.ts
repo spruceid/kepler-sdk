ce8ee70639f10f719f6c7a505c255fe4a0b6a260
Charles Cunningham <charlescunningham@Charless-MBP.fritz.box>
2022-01-20 14:24:18 +0100

    remove compiled code

--- dist/tzString.d.ts
+++
@@ -1,4 +0,0 @@
-import { DAppClient } from '@airgap/beacon-sdk';
-import { Authenticator } from '.';
-export declare const tzStringAuthenticator: <D extends DAppClient>(client: D, domain: string) => Promise<Authenticator>;
-export declare const stringEncoder: (s: string) => string;
