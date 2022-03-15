23b54a71300bdba5fe9d74aa84a44af4371d831e
Charles Cunningham <charlescunningham@Charless-MacBook-Pro.fritz.box>
2022-03-15 23:16:45 +0100

    remove ethString module, improve uri utils

--- src/ethString.ts
+++
@@ -1,7 +0,0 @@
-import { Authenticator, Action, getOrbitId, orbitParams } from '.';
-
-const createEthAuthContentMessage = (orbit: string, pkh: string, action: Action, cids: string[], domain: string): string =>
-    `${domain} ${(new Date()).toISOString()} ${pkh} ${orbit} ${action} ${cids.join(' ')}`
-
-const createEthAuthCreationMessage = async (pkh: string, cids: string[], params: { domain: string; salt?: string; index?: number; }): Promise<string> =>
-    `${params.domain} ${(new Date()).toISOString()} ${pkh} ${await getOrbitId("eth", { address: pkh, ...params })} CREATE eth${orbitParams(params)} ${cids.join(' ')}`
--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,10 @@
 
 
-const makeKRI = (suffix: string, name: string, app?: string, path?: string, fragment?: string) =>
-    `kepler:${suffix}://${name}${app ? ':' + app : ''}${path ? '/' + path : ''}${fragment ? '#' + fragment : ''}`
+export const makeKRI = (suffix: string, name: string, app?: string, path?: string, fragment?: string) =>
+    `kepler:${suffix}://${name}${app ? '/' + app : ''}${path ? '/' + path : ''}${fragment ? '#' + fragment : ''}`
 
-const makeOrbitId = (suffix: string, name: string) => `kepler:${suffix}://${name}`
+export const makeOrbitId = (suffix: string, name: string) => `kepler:${suffix}://${name}`
 
-const getKRI = (orbit: string, app: string, path: string, fragment?: string) => `${orbit}:${app}/${path}${fragment ? '#' + fragment : ''}`
+export const getKRI = (orbit: string, app: string, path: string, fragment?: string) => `${orbit}/${app}/${path}${fragment ? '#' + fragment : ''}`
+
+export const didToOrbitId = (did: string, name: string) => did.startsWith('did:') ? makeOrbitId(did.slice(4), name) : makeOrbitId(did, name)
