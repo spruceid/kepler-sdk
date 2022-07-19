f85e72e8b8a7b4a288ed4c3e4c3ca560cca1a14f
Charles Cunningham <charlescunningham@Charless-MacBook-Pro.fritz.box>
2022-07-19 14:16:24 +0200

    update wasm

--- wasm/web/kepler_sdk_wasm_bg.js
+++ wasm/web/kepler_sdk_wasm_bg.js
@@ -723,8 +723,8 @@ export function __wbindgen_memory() {
     return addHeapObject(ret);
 };
 
-export function __wbindgen_closure_wrapper8526(arg0, arg1, arg2) {
-    const ret = makeMutClosure(arg0, arg1, 3411, __wbg_adapter_26);
+export function __wbindgen_closure_wrapper8540(arg0, arg1, arg2) {
+    const ret = makeMutClosure(arg0, arg1, 3415, __wbg_adapter_26);
     return addHeapObject(ret);
 };
 
