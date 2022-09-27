0b5b84547cff86101bf8664f39819cf83255b49c
Jacob <jacobbward0@gmail.com>
2022-09-27 16:36:13 +0100

    Replace sed with awk in wrapper build script.

--- wrapper/build.sh
+++ wrapper/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tsc -p . && sed -i 's/\(..\/..\/wasm\)/.\/wasm/g' dist/*.d.ts && cp ../wasm/web/kepler_sdk_wasm.d.ts dist/wasm.d.ts
+tsc -p . && awk -i inplace '{gsub(/..\/..\/wasm/, "./wasm");}1' dist/*.d.ts && cp ../wasm/web/kepler_sdk_wasm.d.ts dist/wasm.d.ts
