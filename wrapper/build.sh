1add0993936d1cf135c27e7fbe6e58f6c73d5f55
Jacob <jacobbward0@gmail.com>
2022-09-27 18:17:53 +0100

    Replace awk with js.

--- wrapper/build.sh
+++ wrapper/build.sh
@@ -1,2 +1,19 @@
 #!/bin/sh
-tsc -p . && awk -i inplace '{gsub(/..\/..\/wasm/, "./wasm");}1' dist/*.d.ts && cp ../wasm/web/kepler_sdk_wasm.d.ts dist/wasm.d.ts
+tsc -p . && (
+  echo 'var fs = require("fs");
+const re = new RegExp("../../wasm", "g");
+const dist = fs.opendirSync("./dist");
+while (true) {
+  const entry = dist.readSync();
+  if (!entry) {
+    break;
+  }
+  if (entry.isFile() && entry.name.endsWith(".d.ts")) {
+    let filepath = "./dist/" + entry.name;
+    let file = fs.readFileSync(filepath, { encoding: "utf8" });
+    let replaced = file.replace(re, "./wasm");
+    fs.writeFileSync(filepath, replaced);
+    console.log("rewritten " + filepath);
+  }
+}' | node
+) && cp ../wasm/web/kepler_sdk_wasm.d.ts dist/wasm.d.ts
